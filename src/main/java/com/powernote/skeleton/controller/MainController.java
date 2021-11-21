77aba053fedc16de2e57ba1966504c1102304473
bh kim <bhkim@zinnaworks.com>
2021-11-21 19:08:58 +0900

    thymeleaf 기본 레이아웃 설정

---
+++ src/main/java/com/powernote/skeleton/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.powernote.skeleton.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/main")
+    public String index() {
+        return "pages/main";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "pages/login";
+    }
+}
